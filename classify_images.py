0610180992ef5ccccc3ebdf133b4f573abb49c4d
Nitish Kumar <mintu.nitish@gmail.com>
2019-06-04 11:17:56 +0000

    Checked if image is of a dog

--- classify_images.py
+++ classify_images.py
@@ -75,8 +75,8 @@ def classify_images(images_dir, results_dic, model):
         pet_label = results_dic[file_name][0]
         
         if pet_label in classifier_label:
-            results_dic[file_name].extend([lower_classifier_label, 1])
+            results_dic[file_name].extend((lower_classifier_label, 1))
         else:
-            results_dic[file_name].extend([lower_classifier_label, 0])
+            results_dic[file_name].extend((lower_classifier_label, 0))
         
     None 
