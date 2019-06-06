bc3da13235b25a37041616c4a6c7093ccdf2008c
Nitish Kumar <mintu.nitish@gmail.com>
2019-06-06 15:58:22 +0000

    fixed pet label check with classifier

--- classify_images.py
+++ classify_images.py
@@ -78,5 +78,3 @@ def classify_images(images_dir, results_dic, model):
             results_dic[file_name].extend((lower_classifier_label, 1))
         else:
             results_dic[file_name].extend((lower_classifier_label, 0))
-        
-    None 
