ac97db9f8e3eeb3cd236d3434ddb47b54f6f9fd9
Nitish Kumar <mintu.nitish@gmail.com>
2019-06-06 15:38:16 +0000

    1st review resolved

--- classify_images.py
+++ classify_images.py
@@ -74,7 +74,7 @@ def classify_images(images_dir, results_dic, model):
         
         pet_label = results_dic[file_name][0]
         
-        if pet_label in classifier_label:
+        if pet_label in lower_classifier_label:
             results_dic[file_name].extend((lower_classifier_label, 1))
         else:
             results_dic[file_name].extend((lower_classifier_label, 0))
